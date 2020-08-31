50e4e07f3fd8d4f0a85cca599e1a2f498b354427
Adam Jones <3810108+arkid@users.noreply.github.com>
2020-08-30 20:55:45 -0400

    Clearer comment

--- ACF Repeater Copy-Paste/functions.php
+++ ACF Repeater Copy-Paste/functions.php
@@ -1,6 +1,7 @@
 <?php
 
-// Admin only extra JS/CSS for this code to work
+// Optionally add to your theme functions file to make it load a custom javascript file via the WP admin header on each page load
+
 function acf_repeater_copy_paste_admin_header_inserts() {
 	echo '<script type="text/javascript" src="'.get_stylesheet_directory_uri().'/acf-repeater-copy-paste.js"></script>';
 }
