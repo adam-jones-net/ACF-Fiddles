bade52d6308b1642434f3800543090b4db60e3b7
Adam Jones <3810108+arkid@users.noreply.github.com>
2020-09-13 21:26:58 -0300

    Update functions.php

--- ACF Repeater Copy-Paste/functions.php
+++ ACF Repeater Copy-Paste/functions.php
@@ -1,6 +1,7 @@
 <?php
 
-// Optionally add to your theme functions file to make it load a custom javascript file via the WP admin header on each page load
+// Optionally add the following code to your Wordpress theme's functions.php file to make it load a custom javascript file via the WP admin header on each page load
+// If you already have a method for loading a custom bit of JS into only your admin pages then you likely can ignore this.
 
 function acf_repeater_copy_paste_admin_header_inserts() {
 	echo '<script type="text/javascript" src="'.get_stylesheet_directory_uri().'/acf-repeater-copy-paste.js"></script>';
